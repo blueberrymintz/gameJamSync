69a9d378136273bf4f3299b00e131745554749ec
blueberrymintz <130782323+blueberrymintz@users.noreply.github.com>
2026-05-13 00:42:35 -0700

    committing before bed

--- get-the-hell-out-(unified)/credits_menu.gd
+++ get-the-hell-out-(unified)/credits_menu.gd
@@ -20,6 +20,7 @@ func _ready() -> void:
 
 func _on_button_credits_pressed() -> void:
 	print("menu pressed!")
+	#for some reason calling this one has to be a change scene to file instead of the const and loading
 	get_tree().change_scene_to_file("res://start_menu.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
