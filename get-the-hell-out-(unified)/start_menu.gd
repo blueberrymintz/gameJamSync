09ad59ce526709b519cc954201a0d62d1b59d0ca
blueberrymintz <130782323+blueberrymintz@users.noreply.github.com>
2026-05-12 23:51:30 -0700

    credits menu
    added a credits menu. it still needs to be updated, I added it before asking for everyone's name so I am guessing I will need to make some changes

--- get-the-hell-out-(unified)/start_menu.gd
+++ get-the-hell-out-(unified)/start_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
-const level1 = preload("res://navtest.tscn")
-const levelCredits = preload("res:///credits.tscn")
+const MainMenu = preload("res://start_menu.tscn")
+const Level1 = preload("res://navtest.tscn")
+const LevelCredits = preload("res://credits.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,11 +10,11 @@ func _ready() -> void:
 
 func _on_button_play_pressed() -> void:
 	print("start pressed!")
-	get_tree().change_scene_to_packed(level1)
+	get_tree().change_scene_to_packed(Level1)
 	
 func _on_button_credits_pressed() -> void:
 	print("credits pressed!")
-	get_tree().change_scene_to_packed(levelCredits)
+	get_tree().change_scene_to_packed(LevelCredits)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
