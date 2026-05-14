b82f0722e71be30a0cb80bd0cbc45d655968ad44
PhoenixZombie <egdemet@gmail.com>
2026-05-14 01:26:30 -0700

    Updating Movement

---
+++ get-the-hell-out-(unified)/assets_3d/character_1/character_1.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimationPlayer.queue("idle_animation")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_animation_player_current_animation_changed(name: StringName) -> void:
+	$AnimationPlayer.queue("idle_animation") # Replace with function body.
--- get-the-hell-out-(unified)/player.gd
+++ get-the-hell-out-(unified)/player.gd
@@ -4,7 +4,17 @@ extends Node3D
 @onready var camera = get_viewport().get_camera_3d()
 var currentMode = "select"
 var selectedUnit : Node3D
+var targetLoc: Vector3
+var nextLoc: Vector3
+var pathNode: Node3D
 
+var current_path_index: int = 0
+var current_path_point: Vector3
+var current_path: PackedVector3Array
+@onready var overlayNode = preload("res://assets_3d/overlay_tile.tscn")
+var pathByNode :Array
+
+var funresult
 
 const RAY_LENGTH = 1000.0
 
@@ -51,6 +61,52 @@ func selectUnit(unit):
 			selectedUnit.clearSelect()
 		selectedUnit = unit
 		selectedUnit.select()
+		
+func checkRealDistance(checkPoint: Vector3):
+	var space_state = get_world_3d().direct_space_state
+	var param = PhysicsPointQueryParameters3D.new()
+	param.position = Vector3(checkPoint.x, 0, checkPoint.z)
+	param.collision_mask = 0b1000
+	param.collide_with_areas = true
+	param.collide_with_bodies = false
+	funresult = space_state.intersect_point(param)
+	print(funresult)
+	return funresult
+
+func set_movement_target(target_position: Vector3):
+	if not selectedUnit:
+		return
+	targetLoc = target_position
+	var start_position: Vector3 = selectedUnit.global_position
+	var agent = selectedUnit.get_node("NavigationAgent3D")
+
+	current_path = NavigationServer3D.map_get_path(
+		agent.get_navigation_map(),
+		start_position,
+		target_position,
+		false
+	)
+	pathByNode = []
+	if not current_path.is_empty():
+		current_path_index = 0
+		current_path_point = current_path[0]
+		if pathNode:
+			pathNode.queue_free()
+		pathNode = Node3D.new()
+		add_child(pathNode)
+		print(current_path.size())
+		for pathPoint in current_path:
+			var stuffs = checkRealDistance(pathPoint)
+			for uniqueNode in stuffs:
+				if not uniqueNode.collider_id in pathByNode:
+					pathByNode.append(uniqueNode.collider_id)
+					if pathByNode.size() - 1 < selectedUnit.movement:
+						var instance = overlayNode.instantiate()
+						instance.transform.origin = Vector3(uniqueNode.collider.global_position.x, pathPoint.y, uniqueNode.collider.global_position.y)
+						pathNode.add_child(instance)
+		print(selectedUnit.movement)
+		print(pathByNode.size())
+		
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -59,4 +115,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if $in_level_UI/turnManager.activePlayer.isHuman and currentMode == "move":
+		var mouse_pos = get_viewport().get_mouse_position()
+		var ray_origin = camera.global_position
+		var ray_end = ray_origin + camera.project_ray_normal(mouse_pos) * RAY_LENGTH
+		var param = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+		var result = camera.get_world_3d().direct_space_state.intersect_ray(param)
+		#if result:
+			#print(result.collider.name)
+		if result and selectedUnit and result.collider.get("type") == "terrain":
+			if not targetLoc == result.collider.position:
+				print("settingNewTarget")
+				set_movement_target(result.collider.position)
+			
+		
 	pass
