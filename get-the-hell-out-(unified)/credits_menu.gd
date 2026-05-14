b4f0dcb927448d66007fcc9656c337b1b991558b
blueberrymintz <130782323+blueberrymintz@users.noreply.github.com>
2026-05-13 20:40:51 -0700

    audio manager + main menu + credits
    added in audio manager, main menu and credits. I'm working on making everything set up still. next up will be the user interface stuff

---
+++ get-the-hell-out-(unified)/credits_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+@onready var audio = $audio_manager
+
+const mainMenu = preload("res://start_menu.tscn")
+
+#names
+
+#music
+#vanessa merlis
+
+#producer and writer
+#mason leibel
+
+#programming
+#guthrie demetrios
+#liliana martin
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_button_credits_pressed() -> void:
+	print("menu pressed!")
+	#for some reason calling this one has to be a change scene to file instead of the const and loading
+	get_tree().change_scene_to_file("res://start_menu.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ get-the-hell-out-(unified)/start_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var audio = $audio_manager
+
+
+const MainMenu = preload("res://start_menu.tscn")
+const Level1 = preload("res://main.tscn")
+const LevelCredits = preload("res://credits.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_button_play_pressed() -> void:
+	print("start pressed!")
+	get_tree().change_scene_to_packed(Level1)
+	
+func _on_button_credits_pressed() -> void:
+	print("credits pressed!")
+	get_tree().change_scene_to_packed(LevelCredits)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
