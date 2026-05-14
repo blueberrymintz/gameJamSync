e5bdc6ca453887e031563cfa710eacf34b34b3d1
blueberrymintz <130782323+blueberrymintz@users.noreply.github.com>
2026-05-13 20:56:40 -0700

    added in player buttons
    added in the player buttons to the in level ui

--- get-the-hell-out-(unified)/in_level_ui.gd
+++ get-the-hell-out-(unified)/in_level_ui.gd
@@ -1,6 +1,9 @@
 extends Control
 
 @onready var player = $".."
+@onready var Player1Button = $HBoxContainer/player_1_button
+@onready var Player2Button = $HBoxContainer/player_2_button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -28,6 +31,24 @@ func _next_pressed() -> void:
 	else:
 		player.currentMode = "next"
 	print("next pressed")
+	
+func _player_1_pressed() -> void:
+	#most absolute janky setup. change later
+	#for now it just sets the button that's clicked to disabled
+	#and all the other ones to enabled
+	#should check if the other players are dead in the future
+	
+	Player1Button.grab_focus()
+	
+	print("player 1 pressed")
+	
+	
+	
+func _player_2_pressed() -> void:
+	Player2Button.grab_focus()
+	print("player 2 pressed")
+	
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
